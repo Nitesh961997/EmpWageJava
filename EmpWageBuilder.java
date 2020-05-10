94e9d4cf6ffa8157481d6705774025a9de2c71dd
Nitesh961997 <niteshkavathankar@gmail.com>
2020-05-10 22:42:23 +0530

    Daily Salary available

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,7 +4,7 @@ public class EmpWageComputaion{
 		int fulltime=1;
 		int workHour;
 		int perHrWage=20;
-		double empCheck=(int)Math.floor(Math.random()*10)%2;		//multiplied by 10 because Math.random value is between 0 to 1
+		double empCheck=(int)Math.floor(Math.random()*10)%2;
 		if (empCheck == fulltime)
 		{
 			System.out.println("Employ is present");
