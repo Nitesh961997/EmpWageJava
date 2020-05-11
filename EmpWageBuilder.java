4897ca9ee0595b3a741d88b8f797d4a395e9f845
Nitesh961997 <niteshkavathankar@gmail.com>
2020-05-11 10:55:27 +0530

    Twrnty days work condition added

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,32 +1,71 @@
-public class EmpWageComputaion{
-	public static void main(String[] args)
-		{
-		int fullTime=1;
-		int partTime=2;	
+class Register extends WorkVariables
+{	
+	public void obs() 
+	{
+		int totalSalary=0;
+		int daysWorked=0;
 		int workHour=0;
 		int perHrWage=20;
-		int empCheck=(int)Math.floor(Math.random()*10)%3;		//multiplied by 10 because Math.random value is between 0 to 1
-		switch (empCheck)
+		while(daysWorked<maxdays)
 		{
-		case 1:
-			System.out.println("Fulltime");
-			workHour=8
-			;
-			break;
-		case 2:
-			System.out.println("Parttime");
-			workHour=4
-			;
-			break;
-		case 0:
-			System.out.println("Abscent");
-			workHour=0
-			;
-			break;
-		default:
-			System.out.println("invalid");
-		}	
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
+			System.out.println(empCheck);
+			switch (empCheck)
+			{
+			case 1:
+				System.out.println("Fulltime");
+				workHour=fulltimeHrs;
+				daysWorked=daysWorked+1;
+				;
+				break;
+			case 2:
+				System.out.println("Parttime");
+				workHour=parttimeHrs;
+				daysWorked=daysWorked+1;
+				;
+				break;
+			case 0:
+				System.out.println("Abscent");
+				workHour=abscentHrs;
+				;
+				break;
+			default:
+				System.out.println("invalid");
+			}
+		
 		int dailyWage=workHour*perHrWage;
-		System.out.println("Daily Wage="+dailyWage);
+		totalSalary=totalSalary+dailyWage;
+		}
+		System.out.println(totalSalary);
+	}
+}
+	
+class WorkVariables
+{
+	int fulltime=1;
+	int abscent=0;
+	int parttime=2;
+	int parttimeHrs;
+	int fulltimeHrs;
+	int abscentHrs;
+	int perHrWage;
+	int maxdays;
+	public WorkVariables()
+	{
+		fulltimeHrs=8;
+		abscentHrs=0;
+		perHrWage=20;
+		parttimeHrs=4;
+		maxdays=20;
+				
+		
+	}
+}
+
+public class EmpWageBuilder {
+	public static void main(String[] args)
+		{
+			Register observation=new Register();
+			observation.obs();
 		}
 }
